fce87b46ccf4d5d6530c1c4cd6ebae828195fdde
ControlFlow Graph <controlflowgraph@gmail.com>
2023-01-10 18:28:18 +0100

    fix allocation

--- src/main/java/sloth/Provider.java
+++ src/main/java/sloth/Provider.java
@@ -74,7 +74,7 @@ public class Provider<T>
 
     public boolean hasRemaining(int required)
     {
-        return this.index + required + consumed() <= this.elements.size();
+        return this.index + consumed() <= this.elements.size();
     }
 
     public int size()
