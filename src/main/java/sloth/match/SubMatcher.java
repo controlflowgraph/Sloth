2974a668dec15550588fe4838ba34cc2d729fd5b
ControlFlow Graph <controlflowgraph@gmail.com>
2023-01-10 13:48:57 +0100

    add SubMatcher

---
+++ src/main/java/sloth/match/SubMatcher.java
@@ -0,0 +1,34 @@
+package sloth.match;
+
+import sloth.Provider;
+import sloth.pattern.Pattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public record SubMatcher(String name) implements Matcher
+{
+    @Override
+    public List<Match> match(List<Pattern> patterns, Provider<String> str, List<Match> matches)
+    {
+        List<Match> filtered = new ArrayList<>();
+        for (Match match : matches)
+        {
+            for (Pattern pattern : patterns)
+            {
+                List<Match> t = pattern.tryMatch(patterns, str, match);
+                for (Match m : t)
+                {
+                    filtered.add(match.extend(m.end(), this.name, m));
+                }
+            }
+        }
+        return filtered;
+    }
+
+    @Override
+    public int getMinimumSize()
+    {
+        return 1;
+    }
+}
---
+++ src/test/java/sloth/match/SubMatcherTest.java
@@ -0,0 +1,8 @@
+package sloth.match;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubMatcherTest
+{
+    private final SubMatcher matcher = new SubMatcher("v");
+}
