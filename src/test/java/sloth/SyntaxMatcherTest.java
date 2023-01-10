6c61ce35b227323ab4f15bb8deb548972f1ac2c1
ControlFlow Graph <controlflowgraph@gmail.com>
2023-01-10 18:32:02 +0100

    add formatting to patterns

--- src/test/java/sloth/SyntaxMatcherTest.java
+++ src/test/java/sloth/SyntaxMatcherTest.java
@@ -5,6 +5,7 @@ import sloth.match.*;
 import sloth.pattern.Pattern;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,21 +14,33 @@ class SyntaxMatcherTest
     public static void main(String[] args)
     {
         List<Pattern> patterns = List.of(
-                new Pattern("num", new NumMatcher()),
+                new Pattern("num", new NumMatcher(), m -> {
+                    if(!m.values().containsKey("val"))
+                        return "€";
+                    return m.values().get("val").element().toString();
+                }),
                 new Pattern("times", new SequenceMatcher(List.of(
                         new SubMatcher("a"),
                         new WordMatcher("*"),
                         new SubMatcher("b")
-                )))
+                )),
+                        m -> {
+                            if(!m.values().containsKey("a"))
+                                return "( € * € )";
+                            if(!m.values().containsKey("b"))
+                                return "( " + m.values().get("a").element() + " * € ) ";
+                            return "( " + m.values().get("a").element() + " * " + m.values().get("b").element() + ")";
+                        })
         );
-        Provider<String> provider = new Provider<>(Lexer.lex("10 * 20 * 30"));
+        Provider<String> provider = new Provider<>(Lexer.lex("1 * 2 * 3"));
+
         List<List<Match>> parse = SyntaxMatcher.parse(patterns, provider);
         for (List<Match> matches : parse)
         {
             System.out.println("-".repeat(100));
-            for (Match match : matches)
+            for (int i = 1; i < matches.size(); i++)
             {
-                System.out.println(match);
+                System.out.println(matches.get(i));
             }
         }
     }
