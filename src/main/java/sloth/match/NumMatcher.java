fe6355763067d22501460eee53b2b51f38124ff0
ControlFlow Graph <controlflowgraph@gmail.com>
2023-01-10 13:48:21 +0100

    add NumMatcher

---
+++ src/main/java/sloth/match/NumMatcher.java
@@ -0,0 +1,32 @@
+package sloth.match;
+
+import sloth.Provider;
+import sloth.pattern.Pattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public record NumMatcher() implements Matcher
+{
+    @Override
+    public List<Match> match(List<Pattern> patterns, Provider<String> str, List<Match> matches)
+    {
+        List<Match> filtered = new ArrayList<>();
+        for (Match match : matches)
+        {
+            str.index(match.end());
+            if(str.matches(s -> s.matches("\\d+")))
+            {
+                String value = str.next();
+                filtered.add(match.extend(str.index(), "val", value));
+            }
+        }
+        return filtered;
+    }
+
+    @Override
+    public int getMinimumSize()
+    {
+        return 1;
+    }
+}
