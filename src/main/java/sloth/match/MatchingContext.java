53866fa41b6ff7f51ae5ef176fb714930f96c1b2
ControlFlow Graph <controlflowgraph@gmail.com>
2023-01-11 11:23:26 +0100

    add no args constructor

--- src/main/java/sloth/match/MatchingContext.java
+++ src/main/java/sloth/match/MatchingContext.java
@@ -2,12 +2,18 @@ package sloth.match;
 
 import sloth.pattern.Pattern;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public record MatchingContext(List<Pattern> patterns, Map<String, Meta> info)
 {
+    public MatchingContext()
+    {
+        this(new ArrayList<>());
+    }
+
     public MatchingContext(List<Pattern> patterns)
     {
         this(patterns, new HashMap<>());
