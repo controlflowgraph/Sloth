e2155526d3f1a91bf20fb67ac9de28ef41764379
ControlFlow Graph <controlflowgraph@gmail.com>
2023-01-11 12:50:30 +0100

    filter empty tokens

--- src/main/java/sloth/Lexer.java
+++ src/main/java/sloth/Lexer.java
@@ -3,13 +3,18 @@ package sloth;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.function.Predicate.not;
+
 public class Lexer
 {
     public static List<String> lex(String text)
     {
-        return Arrays.asList(text
-                .replaceAll("([(.)])", " $1 ")
-                .split(" +")
-        );
+        return Arrays.stream(text
+                        .replaceAll("([(.)])", " $1 ")
+                        .split(" +")
+                )
+                .filter(not(String::isBlank))
+                .toList()
+                ;
     }
 }
